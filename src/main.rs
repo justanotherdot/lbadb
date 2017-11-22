debcb014d9d5d33167ca95e52685e95799015313
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-11-22 21:44:21 +1100

    Work on initial repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
+fn print_prompt() {
+    print!("db > ");
+}
+
+fn read_input(input: &mut String) {
+    io::stdin().read_line(input)
+        .expect("Couldn't read line");
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Prompt.
+        print_prompt();
+
+        // Input.
+        let mut input = String::new();
+        read_input(&mut input);
+        println!("{}", input);
+    }
 }
