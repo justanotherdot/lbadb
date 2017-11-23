b4c9e4b15e8d0e42f35ea290ae7b7d1fd46cc72d
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-11-23 21:03:47 +1100

    More changes to the initial lexer and parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use std::io;
 
-fn print_prompt() {
-    print!("db > ");
+struct InputBuffer<'a> {
+    buffer: &'a String,
+    length: usize,
+    capacity: usize,
 }
 
-fn read_input(input: &mut String) {
-    io::stdin().read_line(input)
-        .expect("Couldn't read line");
+fn print_prompt() {
+    use std::io::{self, Write};
+    print!("db> ");
+    io::stdout().flush().unwrap();
+}
+
+fn read_input(input: &mut InputBuffer) {
+    match io::stdin().read_line(input.buffer) {
+        Ok(bytes_read) => {
+            input.length = bytes_read;
+        },
+        Err(err) => panic!("Error reading input: {}", err),
+    };
+    input.buffer.trim();
 }
 
 fn main() {
@@ -15,8 +28,14 @@ fn main() {
         print_prompt();
 
         // Input.
-        let mut input = String::new();
+        let mut input: InputBuffer = InputBuffer {
+            buffer: String::new(),
+            length: 0,
+            capacity: 0,
+        };
         read_input(&mut input);
-        println!("{}", input);
+        if input.buffer == ".exit" {
+            break;
+        }
     }
 }
