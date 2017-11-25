072d2975ae8871b04d136e59c3360d8aa03c3a8e
Ryan James Spencer <spencer.ryanjames@gmail.com>
2017-11-25 21:49:13 +1100

    Integrate part two

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,27 @@ use std::io;
 struct InputBuffer<'a> {
     buffer: &'a mut String,
     length: usize,
+    // NOTE: Due to the way Rust tracks memory, we don't need a capacity field here.
+}
+
+enum MetaCommandResult {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND,
+}
+
+enum PrepareResult {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_COMMAND,
+}
+
+enum StatementType {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT,
+    STATEMENT_INVALID, // Something went wrong setting the type.
+}
+
+struct Statement {
+    tag: StatementType, // Because `type` is a keyword in Rust.
 }
 
 fn print_prompt() {
@@ -14,11 +35,46 @@ fn print_prompt() {
 fn read_input(input: &mut InputBuffer) {
     match io::stdin().read_line(&mut input.buffer) {
         Ok(bytes_read) => {
-            input.length = bytes_read;
+            input.length = bytes_read - 1;
             input.buffer.truncate(bytes_read - 1); // XXX Remove trailing newline.
-        },
+        }
         Err(err) => panic!("Error reading input: {}", err),
     };
+}
+
+fn do_meta_command(input: &mut InputBuffer) -> MetaCommandResult {
+    use std::process;
+    if input.buffer == ".exit" {
+        process::exit(0);
+    } else {
+        MetaCommandResult::META_COMMAND_UNRECOGNIZED_COMMAND
+    }
+}
+
+fn prepare_statement(input: &mut InputBuffer, statement: &mut Statement) -> PrepareResult {
+    if input.length >= 6 && &input.buffer[0..6] == "insert" {
+        statement.tag = StatementType::STATEMENT_INSERT;
+        PrepareResult::PREPARE_SUCCESS
+    } else if input.buffer == "select" {
+        statement.tag = StatementType::STATEMENT_SELECT;
+        PrepareResult::PREPARE_SUCCESS
+    } else {
+        PrepareResult::PREPARE_UNRECOGNIZED_COMMAND
+    }
+}
+
+fn execute_statement(statement: &mut Statement) {
+    match statement.tag {
+        StatementType::STATEMENT_INSERT => {
+            println!("This is where we would do an insert.");
+        },
+        StatementType::STATEMENT_SELECT => {
+            println!("This is where we would do a select.");
+        },
+        StatementType::STATEMENT_INVALID => {
+            println!("Something went wrong: A statement wasn't prepared properly and got passed to be executed.");
+        }
+    }
 }
 
 fn main() {
@@ -32,10 +88,31 @@ fn main() {
             length: 0,
         };
         read_input(&mut input);
-        if input.buffer == ".exit" {
-            break;
-        } else {
-            println!("Unrecognized command: '{}'.", input.buffer);
+
+        // Meta commands.
+        if input.buffer.chars().nth(0).unwrap() == '.' {
+            match do_meta_command(&mut input) {
+                MetaCommandResult::META_COMMAND_SUCCESS => continue,
+                MetaCommandResult::META_COMMAND_UNRECOGNIZED_COMMAND => {
+                    println!("Unrecognized command: '{}'.", input.buffer)
+                }
+            }
+        }
+
+        // Statement preparation.
+        let mut statement: Statement = Statement {
+            tag: StatementType::STATEMENT_INVALID
+        };
+        match prepare_statement(&mut input, &mut statement) {
+            PrepareResult::PREPARE_SUCCESS => {
+                // Statement execution.
+                execute_statement(&mut statement);
+                println!("Executed.");
+            },
+            PrepareResult::PREPARE_UNRECOGNIZED_COMMAND => {
+                println!("Unrecognized command at start of {}", input.buffer);
+                break;
+            }
         }
     }
 }
